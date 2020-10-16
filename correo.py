17dcbbb4f5265857f1363545ae9d7512dc0c364b
JavierHid <Javier.Hidalgo2@alu.uclm.es>
2020-10-16 20:07:28 +0200

    Revert "Revert "Práctica0_Javier_Hidalgo""
    This reverts commit d26222afa86c73f172dc9ec347c648d85c6345b1.

--- correo.py
+++ correo.py
@@ -1,3 +1,4 @@
 # Define aquí mismo la función my_email sin argumentos que
 # devuelve tu correo electrónico de la UCLM
-my_email = 'Javier.Hidalgo2@alu.uclm.es'
+def my_email():
+    return 'javier.hidalgo2@alu.uclm.es'
