e1d712b848e0383626a022dcdefadfc5ff5c5355
JavierHid <Javier.Hidalgo2@alu.uclm.es>
2020-10-16 18:27:59 +0200

    Práctica0_Javier_Hidalgo

--- correo.py
+++ correo.py
@@ -1,3 +1,3 @@
-# Define aquí mismo la función my_email sin argumentos que
+# Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
 my_email = 'Javier.Hidalgo2@alu.uclm.es'
